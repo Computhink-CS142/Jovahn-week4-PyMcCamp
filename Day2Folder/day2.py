031ca9e797ad6315fd90d96033967878d51eb6ee
Computhink-CS142 <cs142@computhink.com.sg>
2025-12-16 07:12:46 +0000

    Dec 16, 2025, 7:12 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -10,7 +10,7 @@ for letter in "g.dragon":
 
 name = "ADAM"
 for letter in name:
-    print("Give me a")        
+    print("Give me a" + "!")        
 
 ########################################################################
 # Task 1:
