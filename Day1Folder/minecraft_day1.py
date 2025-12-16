6a03cb9b59c3f598fcc898dc316e7bb31c96de58
Computhink-CS142 <cs142@computhink.com.sg>
2025-12-16 06:10:14 +0000

    Dec 16, 2025, 6:10 AM

--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,29 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 ################# Function Section #############################
+# comments starts with hashtags
+
+def addition(num1, num2):
+    player.say(num1 * num2)
+
+addition(12, 9)
+
+def teleport():
+    agent.teleport_to_player()
+
+
+
+player.on_chat("come", teleport)
+
+def tr():
+    agent.turn (RIGHT)
+
+player.on_chat("tr", tr)
+   
+
+
+
+
 
 
 ################## On Chat Commands Section #####################
