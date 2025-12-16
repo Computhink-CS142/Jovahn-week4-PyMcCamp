cebdf8072dc15be6f164d1bb2ab298a7c53b0dcd
Computhink-CS142 <cs142@computhink.com.sg>
2025-12-16 06:07:03 +0000

    Dec 16, 2025, 6:07 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,7 +1,13 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
 
+print("hello I am Jovahn.")
+print("I like to swim.")
+print("I like eating burgers!")
+print("I live in Bishan.")
+print("Goodbye!")
+
+print("If I Have $2000, I will save it for something important." )
 ########################################################################
 # Task 1:
 
@@ -10,27 +16,49 @@ print("hello from day1")
 ########################################################################
 # Task 2:
 
-
-
+zoo1=("parrot")
+zoo2=("panda")
+zoo3=("capybara")
+print(zoo1)
+print(zoo2)
+print(zoo3)
 ########################################################################
 # Task 3:
 
-
-
 ########################################################################
 # Task 4:
+var1 = 10
+var2 = 2
+print(var1+var2)
+print(var1/var2)
+print(var1-var2)
+print(var1*var2)
+name="Dog"
+food="bone"
+print("My name is name, name likes to eat food")
 
 
 
 ########################################################################
 # Task 5:
 
+def greetings():
+    print("Hello")
+    print("My name is Jovahn")
+    print("I like to swim")
 
+greetings() # call a function
+greetings() # call a function
 
 ########################################################################
 # Task 6:
 
+def intro(Jovahn):
+    print("Nice to meet you,"+ Jovahn)  # whatever is ignore
+    print(Jovahn) 
 
+intro("George")
+intro("Betty")     
 
 ########################################################################
 # Task 7:
