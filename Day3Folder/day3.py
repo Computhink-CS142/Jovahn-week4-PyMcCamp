5b6a70e86a370ef7c743565ca8e91002c8d8b19f
Computhink-CS142 <cs142@computhink.com.sg>
2025-12-17 07:38:40 +0000

    Dec 17, 2025, 7:38 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -24,7 +24,7 @@ print(title, yourname, "orders the peasents to", task)
 #calculator
 
 num1 = input("what is the first number?")
-num2 = input("what is the first number?")
+num2 = input("what is the second number?")
 
 ########################################################################
 # Task 4:
