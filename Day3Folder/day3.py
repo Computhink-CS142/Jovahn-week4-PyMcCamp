1b550a99f5ad20374b554e65d0980842709c6256
Computhink-CS142 <cs142@computhink.com.sg>
2025-12-17 07:30:59 +0000

    Dec 17, 2025, 7:30 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -6,7 +6,7 @@
 
 yourname = input("What is your name?")
 title = input("What is your title?")
-task = input
+task = input("What is ")
 print(title, yourname, "orders the peasents to", task)
  # type: ignore
 
